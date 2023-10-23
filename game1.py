c466bd8d2c4e7ea721f4edbc3a3424e557117364
03230383 <03230383.gcbs@rub.edu.bt>
2023-10-23 19:20:40 +0600

    added coded

--- game1.py
+++ game1.py
@@ -5,3 +5,21 @@ sc = turtle.Screen()
 sc.title("Pong game")
 sc.bgcolor("pink")
 sc.setup(width=1000, height=600)
+
+# Left paddle
+left_pad = turtle.Turtle()
+left_pad.speed(0)
+left_pad.shape("square")
+left_pad.color("purple")
+left_pad.shapesize(stretch_wid=6, stretch_len=2)
+left_pad.penup()
+left_pad.goto(-400, 0)
+
+# Right paddle
+right_pad = turtle.Turtle()
+right_pad.speed(0)
+right_pad.shape("square")
+right_pad.color("purple")
+right_pad.shapesize(stretch_wid=6, stretch_len=2)
+right_pad.penup()
+right_pad.goto(400, 0)
